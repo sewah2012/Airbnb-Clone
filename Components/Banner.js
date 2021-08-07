f6ada1ed9225b5c80bb87f856fe20eb4b88d52b1
Emmanuel_Sewah <sewah2012@gmail.com>
2021-08-07 09:58:32 +0000

    fix duplicate styling in banner

--- Components/Banner.js
+++ Components/Banner.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 const Banner = () => {
   return (
-    <div className="relative h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] xl:h-[800px] 2xl:h-[900px] ">
+    <div className="relative h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[900px] ">
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
